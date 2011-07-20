1dc2c3e7f80070aae8e78868cc23ed9b3eb46a72
Christopher Jeffrey <chjjeffrey@gmail.com>
2011-07-20 00:56:27 -0500

    fix n-features

--- test/test-features.js
+++ test/test-features.js
@@ -1,1 +1,34 @@
-var fs = require('fs');var $N = require('../lib/N-features');$N(function() {  fs.readFile('./log1.txt', 'utf-8', this);}).N(function(err, text) {  if (err) {    console.log(err);   } else {     console.log('Some text: ' + text);  }  this.N(    // you can even push     // more functions onto the stack!    function(err, results) {      console.log('world');      this(results);    },     // you can pass multiple args if     // you want to use it like Step    function(err, results) {       console.log('how are you?');      console.log('...even more text:', results);    }  )('hello'); }).N(function(err, hello) {  if (err) console.log('oh no, an error:', err)  console.log(hello);  fs.readFile('./log2.txt', 'utf-8', this.para);  fs.readFile('./log3.txt', 'utf-8', this.para);})(); 
+var fs = require('fs');
+
+var $N = require('../lib/N-features');
+
+$N(function() {
+  fs.readFile('./log1.txt', 'utf8', this);
+})
+.N(function(err, text) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Some text: ' + text);
+  }
+  this.N(
+    // you can even push
+    // more functions onto the stack!
+    function(err, results) {
+      console.log('world');
+      this(results);
+    },
+    // you can pass multiple args if
+    // you want to use it like Step
+    function(err, results) {
+      console.log('how are you?');
+      console.log('...even more text:', results);
+    }
+  )('hello');
+})
+.N(function(err, hello) {
+  if (err) console.log('oh no, an error:', err)
+  console.log(hello);
+  fs.readFile('./log2.txt', 'utf8', this.para);
+  fs.readFile('./log3.txt', 'utf8', this.para);
+})();
--- test/test.js
+++ test/test.js
@@ -3,24 +3,24 @@ var fs = require('fs');
 var $N = require('../');
 
 $N(function(next) {
-  fs.readFile('./log.txt', 'utf-8', next);
+  fs.readFile('./log.txt', 'utf8', next);
 })
 .N(function(next, err, txt) {
   if (err) {
-    console.log(err.message); 
-  } else { 
+    console.log(err.message);
+  } else {
     console.log('Some text: ' + txt);
   }
   next.N(
-    // you can even push 
+    // you can even push
     // more functions onto the stack!
-    function(next) { 
+    function(next) {
       console.log('world');
       next();
-    }, 
-    // you can pass multiple args if 
+    },
+    // you can pass multiple args if
     // you want to use it like Step
-    function() { 
+    function() {
       console.log('how are you?');
     }
   )('hello');
@@ -28,4 +28,4 @@ $N(function(next) {
 .N(function(next, hello) {
   console.log(hello);
   next();
-})(); 
+})();
